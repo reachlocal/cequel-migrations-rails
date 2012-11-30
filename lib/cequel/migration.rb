6082b731f957c9548dfb0db904b7db3433295121
Andrew De Ponte <cyphactor@gmail.com>
2012-11-30 08:27:13 -0800

    Started down the path of adding the 'execute' func
    I did this as it is really core of the entire Cequel::Migration class as once
    the connection creation is handled and the execute method works then it is
    just a matter of building the nice little helper methods on top of the execute
    method.

--- lib/cequel/migration.rb
+++ lib/cequel/migration.rb
@@ -1,4 +1,18 @@
 module Cequel
   class Migration
+    attr_reader :db
+
+    def initialize
+      @db = CassandraCQL::Database.new(self.class.cequel_env_conf['host'])
+      @db.execute("USE #{self.class.cequel_env_conf['keyspace']}")
+    end
+
+    def execute(cql_string)
+      db.execute(cql_string)
+    end
+
+    def self.cequel_env_conf
+      YAML::load(File.open(File.join(::Rails.root,"config", "cequel.yml")))[::Rails.env]
+    end
   end
 end
--- spec/lib/cequel/migration_spec.rb
+++ spec/lib/cequel/migration_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe Cequel::Migration do
+  let(:migration_class) { class FooMigration < Cequel::Migration; end; FooMigration }
+  let(:migration) { migration_class.new }
+
+  describe "#execute" do
+    it "delegates to the cassandra-cql connection execute" do
+      migration_class.stub(:cequel_env_conf).and_return({ 'keyspace' => 'test keyspace', 'host' => '123.123.123.123' })
+      db = mock('db').as_null_object
+      CassandraCQL::Database.stub(:new).and_return(db)
+      db.should_receive(:execute).with("some cql string")
+      migration.execute("some cql string")
+    end
+  end
 end
