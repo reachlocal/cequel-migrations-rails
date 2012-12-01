d89e515d85d6d7d429192a40bddd94bfac13d0ba
Andrew De Ponte <cyphactor@gmail.com>
2012-11-30 16:51:06 -0800

    Backfilled new specs for Cequel::Migration

--- spec/lib/cequel/migration_spec.rb
+++ spec/lib/cequel/migration_spec.rb
@@ -4,6 +4,22 @@ describe Cequel::Migration do
   let(:migration_class) { class FooMigration < Cequel::Migration; end; FooMigration }
   let(:migration) { migration_class.new }
 
+  describe "#new" do
+    it "create a cassandra-cql database connection for the host specified in the environment's config" do
+      migration_class.stub(:cequel_env_conf).and_return({ 'host' => 'somehost' })
+      CassandraCQL::Database.should_receive(:new).with('somehost').and_return(stub('db', :execute => nil))
+      migration
+    end
+
+    it "use the keyspace specified in the environment's config" do
+      migration_class.stub(:cequel_env_conf).and_return({ 'host' => 'somehost', 'keyspace' => 'somekeyspace' })
+      db = mock('db')
+      CassandraCQL::Database.stub(:new).and_return(db)
+      db.should_receive(:execute).with("USE somekeyspace") 
+      migration
+    end
+  end
+  
   describe "#execute" do
     it "delegates to the cassandra-cql connection execute" do
       migration_class.stub(:cequel_env_conf).and_return({ 'keyspace' => 'test keyspace', 'host' => '123.123.123.123' })
