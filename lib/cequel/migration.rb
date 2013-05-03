d63d5d9d55b03c4b6ab5c02626076221adbff9bc
Mike West <mwest@mdsol.com>
2013-05-03 13:33:55 -0400

    Update so we use hosts value if present

--- lib/cequel/migration.rb
+++ lib/cequel/migration.rb
@@ -3,7 +3,7 @@ module Cequel
     attr_reader :db
 
     def initialize
-      @db = CassandraCQL::Database.new(self.class.cequel_env_conf['host'], { :keyspace => self.class.cequel_env_conf['keyspace'] })
+      @db = CassandraCQL::Database.new(servers, { :keyspace => self.class.cequel_env_conf['keyspace'] })
     end
 
     def execute(cql_string)
@@ -12,6 +12,11 @@ module Cequel
 
     def self.cequel_env_conf
       YAML::load(File.open(File.join(::Rails.root,"config", "cequel.yml")))[::Rails.env]
+    end
+
+  private
+    def servers
+      self.class.cequel_env_conf['hosts'] || self.class.cequel_env_conf['host']
     end
   end
 end
