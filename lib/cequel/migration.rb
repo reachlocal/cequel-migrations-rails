49062deed5875c492d00404be182f12d7cac2378
Andrew De Ponte <cyphactor@gmail.com>
2013-07-03 19:07:56 -0700

    Made it so that it handles the thrift options.
    I did this so that we could control the thrift timeouts etc.

--- lib/cequel/migration.rb
+++ lib/cequel/migration.rb
@@ -3,7 +3,7 @@ module Cequel
     attr_reader :db
 
     def initialize
-      @db = CassandraCQL::Database.new(servers, { :keyspace => self.class.cequel_env_conf['keyspace'] })
+      @db = CassandraCQL::Database.new(servers, { :keyspace => self.class.cequel_env_conf['keyspace'] }, thrift_options)
     end
 
     def execute(cql_string)
@@ -17,6 +17,14 @@ module Cequel
   private
     def servers
       self.class.cequel_env_conf['hosts'] || self.class.cequel_env_conf['host']
+    end
+
+    def thrift_options
+      if self.class.cequel_env_conf['thrift']
+        return self.class.cequel_env_conf['thrift'].inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+      else
+        return {}
+      end
     end
   end
 end
